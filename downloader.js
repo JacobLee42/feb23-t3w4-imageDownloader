a22097f4b83fdb75ce13a6089eacd1a6423c886d
JacobLee42 <14303@coderacademy.edu.au>
2023-09-09 10:46:06 +1000

    feature of URL done

--- downloader.js
+++ downloader.js
@@ -1,3 +1,25 @@
+// Sychronous library for doing file IO
+// check if directory exists
+
+const fs = require("node:fs")
+
+
+
+
+const {mkdir} = require("node:fs/promises")
+
+// streaming data. safer than traditional file saving
+// This is sychronous, so it is  blocking
+const {Readable} = require("node:stream");
+
+const {finished} = require("node:stream/promises")
+
+// Node file & directory path helper system
+
+
+
+const path = require("node:path")
+
 
 
 
@@ -12,14 +34,40 @@ function getRandomPokemonId(){
 
 // retrieve Pokemon data for that number
 // retrieve the image URL from the pokemon data
+async function getPokemonPictureUrl(targetId = getRandomPokemonId()){
+    let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + targetId).catch(error => {
+        throw new Error("API failure.");
+});
+
+
+    if (response.status == "404"){
+        throw new Error("API did not have data for the requested ID.");
+    }
 
 
 
+    let data = await response.json().catch(error => {
+        throw new Error("API did not return valid JSON.");
+    })
+}
+
+    return data.sprites.other["official-artwork"].front_default;
+    
 
 
 //download that image and save it to the computer
 //return download images file path
+async function savePokemonPictureToDisk(targetUrl, targetDownloadFilename, targetDownloadDirectory = "."){
+
+}
+
+
+
+
 
 module.exports = {
-    downloadPokemonPicture
+    downloadPokemonPicture,
+    savePokemonPictureToDisk,
+    getPokemonPictureUrl,
+    getRandomPokemonId
 }
