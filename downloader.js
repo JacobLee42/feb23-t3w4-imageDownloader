cf373916d196b0b7edaf520d8a3701a25b1fd049
JacobLee42 <14303@coderacademy.edu.au>
2023-09-09 10:07:04 +1000

    initial setup

---
+++ downloader.js
@@ -0,0 +1,25 @@
+
+
+
+function downloadPokemonPicture (targetId = getRandomPokemonId()){
+
+}
+// generate a random number or use a user-provided number
+function getRandomPokemonId(){
+    return Math.floor(Math.random() * 1010) + 1
+}
+
+
+// retrieve Pokemon data for that number
+// retrieve the image URL from the pokemon data
+
+
+
+
+
+//download that image and save it to the computer
+//return download images file path
+
+module.exports = {
+    downloadPokemonPicture
+}
